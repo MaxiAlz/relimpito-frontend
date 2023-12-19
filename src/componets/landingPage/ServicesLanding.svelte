7bc0caad0dc18f41e8104d55976443a683dadd3b
Maxi Alzugaray <maxialz99@hotmail.com>
2023-12-19 16:50:02 -0300

    seccion de servicios en landing page

---
+++ src/componets/landingPage/ServicesLanding.svelte
@@ -0,0 +1,160 @@
+<h1 class="text-primary font-semibold text-2xl text-center uppercase mb-10">
+  Nuestros servicios
+</h1>
+
+<section class="flex flex-col lg:flex-row justify-center">
+  <!-- Proveedor de recursos / productos  para empresas y negocios-->
+  <!-- Atencion online: Realia tu pedido de manera online, espera confirmacion del pago y recibilo en tu domicilio -->
+  <!-- Logistica de entrega asegurada -->
+  <div
+    class="flex flex-grow card bg-base-200 rounded-box place-items-center my-5 mx-2 p-5 text-center"
+  >
+    <svg
+      width="70px"
+      height="70px"
+      viewBox="0 0 512 512"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="#000000"
+      ><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g
+        id="SVGRepo_tracerCarrier"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      ></g><g id="SVGRepo_iconCarrier"
+        ><line
+          fill="none"
+          stroke="#5e5e5e"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="32"
+          x1="448"
+          y1="448"
+          x2="448"
+          y2="240"
+        ></line><line
+          fill="none"
+          stroke="#5e5e5e"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="32"
+          x1="64"
+          y1="240"
+          x2="64"
+          y2="448"
+        ></line><path
+          fill="none"
+          stroke="#5e5e5e"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="32"
+          d="M382.47,48H129.53C107.74,48,88.06,60,79.6,78.46L36.3,173c-14.58,31.81,9.63,67.85,47.19,69q1,0,2,0c31.4,0,56.85-25.18,56.85-52.23,0,27,25.46,52.23,56.86,52.23S256,218.62,256,189.77c0,27,25.45,52.23,56.85,52.23s56.86-23.38,56.86-52.23c0,28.85,25.45,52.23,56.85,52.23q1,0,1.95,0c37.56-1.17,61.77-37.21,47.19-69L432.4,78.46C423.94,60,404.26,48,382.47,48Z"
+        ></path><line
+          fill="none"
+          stroke="#5e5e5e"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="32"
+          x1="32"
+          y1="464"
+          x2="480"
+          y2="464"
+        ></line><path
+          fill="none"
+          stroke="#5e5e5e"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="32"
+          d="M136,288h80a24,24,0,0,1,24,24v88a0,0,0,0,1,0,0H112a0,0,0,0,1,0,0V312A24,24,0,0,1,136,288Z"
+        ></path><path
+          fill="none"
+          stroke="#5e5e5e"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="32"
+          d="M288,464V312a24,24,0,0,1,24-24h64a24,24,0,0,1,24,24V464"
+        ></path></g
+      ></svg
+    >
+    <span class="text-primary font-bold uppercase">Proveemos recursos </span>
+    <p class="m-2">
+      Mantenemos el stock de producto actualizado durante todo el año para tu
+      empresa, negocio y oficina.
+    </p>
+  </div>
+  <div class="divider divider-horizontal"></div>
+  <div
+    class="flex flex-grow card bg-base-200 rounded-box place-items-center my-5 mx-2 p-5 text-center"
+  >
+    <svg
+      fill="#5e5e5e"
+      height="65px"
+      width="65px"
+      version="1.1"
+      id="Layer_1"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlns:xlink="http://www.w3.org/1999/xlink"
+      viewBox="0 0 491.52 491.52"
+      xml:space="preserve"
+      ><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g
+        id="SVGRepo_tracerCarrier"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      ></g><g id="SVGRepo_iconCarrier">
+        <g>
+          <g>
+            <path
+              d="M444.42,222.43l-94.045-25.65l25.645,94.04l26.964-26.961l23.531,23.531l14.48-14.48l-23.529-23.529L444.42,222.43z M386.5,251.38l-6.935-25.42l25.425,6.93L386.5,251.38z"
+            ></path>
+          </g>
+        </g>
+        <g>
+          <g>
+            <path
+              d="M491.52,409.6V0H0v409.6h153.6v61.44h-40.96v20.48h266.24v-20.48h-40.96V409.6H491.52z M20.48,20.48h450.56v317.44H20.48 V20.48z M317.44,471.04H174.08V409.6h143.36V471.04z M20.48,389.12V358.4h450.56v30.72H20.48z"
+            ></path>
+          </g>
+        </g>
+        <g>
+          <g>
+            <path
+              d="M294.1,215.04l37.545-112.64H102.084L90.155,63.63L54.44,51.73l-6.48,19.42l25.725,8.58l38.203,124.155L85.83,256h28.697 c-1.147,3.219-1.887,6.632-1.887,10.24c0,16.94,13.78,30.72,30.72,30.72s30.72-13.78,30.72-30.72c0-3.608-0.739-7.021-1.887-10.24 h44.734c-1.148,3.219-1.887,6.632-1.887,10.24c0,16.94,13.78,30.72,30.72,30.72s30.72-13.78,30.72-30.72 c0-3.608-0.739-7.021-1.887-10.24h12.127v-20.48H118.97l10.24-20.48H294.1z M245.76,256c5.645,0,10.24,4.59,10.24,10.24 c0,5.65-4.595,10.24-10.24,10.24s-10.24-4.59-10.24-10.24C235.52,260.59,240.115,256,245.76,256z M143.36,256 c5.645,0,10.24,4.59,10.24,10.24c0,5.65-4.595,10.24-10.24,10.24c-5.645,0-10.24-4.59-10.24-10.24 C133.12,260.59,137.715,256,143.36,256z M108.386,122.88h194.849l-23.895,71.68H130.441L108.386,122.88z"
+            ></path>
+          </g>
+        </g>
+      </g></svg
+    >
+    <span class="text-primary font-bold uppercase">Atencion online</span>
+    <p class="m-2">
+      Realia tu pedido de manera online, envia el detalle, espera confirmacion
+      del pago y recibilo en tu domicilio.
+    </p>
+  </div>
+  <div class="divider divider-horizontal"></div>
+  <div
+    class="flex flex-grow card bg-base-200 rounded-box place-items-center my-5 mx-2 p-5 text-center"
+  >
+    <svg
+      width="75px"
+      height="75px"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      ><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g
+        id="SVGRepo_tracerCarrier"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      ></g><g id="SVGRepo_iconCarrier">
+        <path
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+          d="M16.5 6H3V17.25H3.375H4.5H4.52658C4.70854 18.5221 5.80257 19.5 7.125 19.5C8.44743 19.5 9.54146 18.5221 9.72342 17.25H15.0266C15.2085 18.5221 16.3026 19.5 17.625 19.5C18.9474 19.5 20.0415 18.5221 20.2234 17.25H21.75V12.4393L18.3107 9H16.5V6ZM16.5 10.5V14.5026C16.841 14.3406 17.2224 14.25 17.625 14.25C18.6721 14.25 19.5761 14.8631 19.9974 15.75H20.25V13.0607L17.6893 10.5H16.5ZM15 15.75V9V7.5H4.5V15.75H4.75261C5.17391 14.8631 6.07785 14.25 7.125 14.25C8.17215 14.25 9.07609 14.8631 9.49739 15.75H15ZM17.625 18C17.0037 18 16.5 17.4963 16.5 16.875C16.5 16.2537 17.0037 15.75 17.625 15.75C18.2463 15.75 18.75 16.2537 18.75 16.875C18.75 17.4963 18.2463 18 17.625 18ZM8.25 16.875C8.25 17.4963 7.74632 18 7.125 18C6.50368 18 6 17.4963 6 16.875C6 16.2537 6.50368 15.75 7.125 15.75C7.74632 15.75 8.25 16.2537 8.25 16.875Z"
+          fill="#5e5e5e"
+        ></path>
+      </g></svg
+    >
+    <span class="text-primary font-bold uppercase">Entrega asegurada</span>
+    <p class="m-2">
+      Realiza una compras superando un monto minimo y recibi los productos en tu
+      domicilio.
+    </p>
+  </div>
+</section>
