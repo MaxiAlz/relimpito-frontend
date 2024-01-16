2d946968c69451d40f1ec4493cbcc1fd16db9d0d
Maxi Alzugaray <maxialz99@hotmail.com>
2024-01-16 19:29:04 -0300

    agrego componente para el manejo de informacion de compra

---
+++ src/componets/shoppingCart/OrderDetail.svelte
@@ -0,0 +1,35 @@
+<script>
+  import { onMount } from "svelte";
+  import { httpRequest } from "../../helpers/httpRequest";
+  import SelectUserAdressForOrders from "../users/SelectUserAdressForOrders.svelte";
+
+  let isLoading = false;
+  let adresses = [];
+  let selectedAdress = {};
+  let { street, number, postalCode, city, location } = selectedAdress;
+  // onMount(() => {
+  //   getUSerAdress;
+  // });
+  $: console.log("adresses :>> ", adresses);
+  $: console.log("selectedAdress :>> ", selectedAdress);
+
+  const getUSerAdress = async () => {
+    isLoading = true;
+    try {
+      const { data } = await httpRequest(`/users/me?populate=adresses`, "GET");
+      // console.log("getUSerAdress :>> ", data.adresses);
+      adresses = data.adresses;
+      selectedAdress = data.adresses[0];
+    } catch (error) {
+      console.error("Error al obtener productos :>> ", error);
+    }
+    isLoading = false;
+  };
+</script>
+
+<div class="card w-full flex m-2">
+  <!-- Detalles del envio -->
+
+  <h1>Elegí la forma de entrega</h1>
+  <SelectUserAdressForOrders />
+</div>
