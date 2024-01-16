44642307d7a9041863898a6ebebebbcd603d05fb
Maxi Alzugaray <maxialz99@hotmail.com>
2024-01-16 19:28:43 -0300

    creo componentes para agregar detalles en la compra

--- src/routes/ViewMyShoppingCart.svelte
+++ src/routes/ViewMyShoppingCart.svelte
@@ -10,9 +10,11 @@
   } from "../stores/shoppingCart";
   import { notifications } from "../componets/alertsUser/alert";
   import { navigate } from "svelte-routing";
+  import OrderDetail from "../componets/shoppingCart/OrderDetail.svelte";
 
   onMount(() => {
     isFirstloading = true;
+    console.log("primera vez :>> ");
     itemsLocalStorageCart = getShoppingCartLocarStorage() || [];
     if (itemsLocalStorageCart.length > 0) {
       isUpdating = true;
@@ -28,6 +30,9 @@
   let totalPriceProducts = 0;
   let purchaseProductDetail = {}; //aca guardo el detalle completo de la compra (productos, precios, detalles, etc)
   let amountMessageError = {};
+
+  let buySteps = [];
+  let currentStep = 0;
 
   const setPurchaseDetail = (productsInCart) => {
     // crea un objeto con todos los detalles de la compra por fila de producto, agrega cantidad y calcula el precio parcial
@@ -58,6 +63,8 @@
 
     calculateTotalPriceOfPurchase();
   };
+
+  $: console.log("purchaseProductDetail :>> ", purchaseProductDetail);
 
   const calculateTotalPriceOfPurchase = () => {
     //logica para calcular todos los recargos que puedan haber y colocarlos en purchaseProductDetail y luego retornar total
@@ -162,104 +169,117 @@
     </div>
   {/if}
   {#if !isFirstloading && purchaseProductDetail.productDetails}
-    <div class="card w-full flex m-2">
-      <table class="table overflow-x-auto shadow">
-        <!-- head -->
-
-        <thead>
-          <tr>
-            <th>Codigo</th>
-            <th>Articulo</th>
-            <th>Cantidad</th>
-            <th>Precio</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {#each purchaseProductDetail.productDetails as { attributes, totalPrice, amount, id } (id)}
+    {#if currentStep === 0}
+      <div class="card w-full flex m-2">
+        <table class="table overflow-x-auto shadow">
+          <thead>
             <tr>
-              <td>
-                <span class="text-primary font-semibold"
-                  >{attributes.productCode}</span
-                >
-              </td>
-              <td>
-                <div class="flex flex-col items-start">
-                  <button
-                    on:click={() => navigate(`/Articulos/Ver-Producto/${id}`)}
+              <th>Codigo</th>
+              <th>Articulo</th>
+              <th>Cantidad</th>
+              <th>Precio</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {#each purchaseProductDetail.productDetails as { attributes, totalPrice, amount, id } (id)}
+              <tr>
+                <td>
+                  <span class="text-primary font-semibold"
+                    >{attributes.productCode}</span
                   >
-                    <span
-                      class="text-neutral uppercase font-semibold hover:text-primary"
-                      >{attributes.productName}</span
+                </td>
+                <td>
+                  <div class="flex flex-col items-start">
+                    <button
+                      on:click={() => navigate(`/Articulos/Ver-Producto/${id}`)}
                     >
-                  </button>
-                  <button class="btn btn-ghost text-primary btn-xs mt-1"
-                    >Eliminar</button
-                  >
-                </div>
-              </td>
-              <td class="flex flex-col items-start">
-                <div class="join">
-                  <button
-                    class="btn btn-sm join-item"
-                    disabled={amount <= 0}
-                    on:click={() => updateRemoveAnAmountProduct(id)}>-</button
-                  >
-                  <input
-                    type="text"
-                    class="input input-sm join-item w-16 text-center font-semibold"
-                    on:blur={(e) =>
-                      setAmountProductByManopla(id, e, attributes.stock)}
-                    on:keydown={(e) => userPressEnter(id, e, attributes.stock)}
-                    value={amount}
-                  />
-                  <button
-                    class="btn btn-sm join-item"
-                    disabled={amount >= attributes.stock}
-                    on:click={() => updateIncreaseAnAmountProduct(id)}>+</button
-                  >
-                </div>
+                      <span
+                        class="text-neutral uppercase font-semibold hover:text-primary"
+                        >{attributes.productName}</span
+                      >
+                    </button>
+                    <button class="btn btn-ghost text-primary btn-xs mt-1"
+                      >Eliminar</button
+                    >
+                  </div>
+                </td>
+                <td class="flex flex-col items-start">
+                  <div class="join">
+                    <button
+                      class="btn btn-sm join-item"
+                      disabled={amount <= 0}
+                      on:click={() => updateRemoveAnAmountProduct(id)}>-</button
+                    >
+                    <input
+                      type="text"
+                      class="input input-sm join-item w-16 text-center font-semibold"
+                      on:blur={(e) =>
+                        setAmountProductByManopla(id, e, attributes.stock)}
+                      on:keydown={(e) =>
+                        userPressEnter(id, e, attributes.stock)}
+                      value={amount}
+                    />
+                    <button
+                      class="btn btn-sm join-item"
+                      disabled={amount >= attributes.stock}
+                      on:click={() => updateIncreaseAnAmountProduct(id)}
+                      >+</button
+                    >
+                  </div>
 
-                <div
-                  class={amountMessageError.id === id && amountMessageError.msg
-                    ? "tooltip tooltip-open tooltip-bottom tooltip-error"
-                    : null}
-                  data-tip={amountMessageError.msg}
-                >
-                  <span
+                  <div
                     class={amountMessageError.id === id &&
                     amountMessageError.msg
-                      ? "text-sm text-center text-error font-semibold"
-                      : "opacity-80 text-sm text-center"}
-                    >{attributes.stock} disponibles
-                  </span>
-                </div>
-              </td>
-              <td>
-                {#if isUpdating}
-                  <div class="w-full flex justify-center">
+                      ? "tooltip tooltip-open tooltip-bottom tooltip-error"
+                      : null}
+                    data-tip={amountMessageError.msg}
+                  >
                     <span
-                      class="loading loading-spinner text-primary loading-lg"
-                    ></span>
+                      class={amountMessageError.id === id &&
+                      amountMessageError.msg
+                        ? "text-sm text-center text-error font-semibold"
+                        : "opacity-80 text-sm text-center"}
+                      >{attributes.stock} disponibles
+                    </span>
                   </div>
-                {/if}
-                {#if !isUpdating}
-                  <div class="flex flex-col">
-                    <span class="text-neutral uppercase font-semibold text-lg"
-                      >${totalPrice},00</span
-                    >
-                    <span class="opacity-80 text-sm">
-                      ${attributes.wholesalePrice},00 c/u</span
-                    >
-                  </div>
-                {/if}
-              </td>
-            </tr>
-          {/each}
-        </tbody>
-      </table>
-    </div>
+                </td>
+                <td>
+                  {#if isUpdating}
+                    <div class="w-full flex justify-center">
+                      <span
+                        class="loading loading-spinner text-primary loading-lg"
+                      ></span>
+                    </div>
+                  {/if}
+                  {#if !isUpdating}
+                    <div class="flex flex-col">
+                      <span class="text-neutral uppercase font-semibold text-lg"
+                        >${totalPrice},00</span
+                      >
+                      <span class="opacity-80 text-sm">
+                        ${attributes.wholesalePrice},00 c/u</span
+                      >
+                    </div>
+                  {/if}
+                </td>
+              </tr>
+            {/each}
+          </tbody>
+        </table>
 
+        <button
+          class="btn btn-primary btn-outline m-2 w-1/3"
+          on:click={() => navigate("/Catalogo")}>Ver catalogo completo</button
+        >
+      </div>
+    {/if}
+
+    {#if currentStep === 1}
+      <OrderDetail />
+    {/if}
+
+    <!-- Card Resumen de compra -->
     <div class="card m-2 bg-base-100 shadow box-border w-1/3">
       <div class="card-body">
         <h2 class="card-title">Resumen de compra:</h2>
@@ -293,7 +313,8 @@
         <div class="card-actions">
           <button
             class="btn btn-primary w-full"
-            disabled={amountMessageError.msg}>Continuar compra</button
+            disabled={amountMessageError.msg}
+            on:click={() => (currentStep += 1)}>Continuar compra</button
           >
         </div>
       </div>
@@ -305,8 +326,9 @@
     </div>
   {/if}
 </section>
+
 <section class="flex flex-col items-center">
-  {#if isFirstloading && !purchaseProductDetail.productDetails}
+  {#if !isFirstloading && !itemsLocalStorageCart.length}
     <h2 class="text-center font-semibold text-2xl">
       Tu carrito esta vacio <span class="material-symbols-outlined">
         sentiment_sad
